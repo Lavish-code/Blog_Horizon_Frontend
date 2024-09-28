0c42bdb4a2e8adb8c7b46ad4f30c12142957026c
Lavish-code <ylavish07@gmail.com>
2024-09-29 02:01:25 +0530

    Your commit message describing the changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,10 +4,7 @@ const Loader = () => {
   return (
     <div className='loader'>
         <div className="loader__image">
-            <div className="tenor-gif-embed" data-postid="25192894" data-share-method="host" data-aspect-ratio="1" data-width="100%">
-                <a href="https://tenor.com/view/amalie-steiness-borregaard-loading-gif-loading-gif-25192894">Amalie Steiness GIF</a> from <a href="https://tenor.com/search/amalie-gifs">Amalie GIFs</a>
-            </div>
-            <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+            <img src="https://media.tenor.com/images/a651d8e9b64cb2139e5a9e1d77da18f4/tenor.gif" alt="Loading..." />
         </div>
     </div>
   );
