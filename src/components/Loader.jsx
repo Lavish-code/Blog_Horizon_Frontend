03a84507cad45ee7f5093f49be1930e3925c7e2d
Lavish-code <ylavish07@gmail.com>
2024-09-29 01:58:31 +0530

    Your commit message describing the changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import LoadingGif from '../images/loader.gif'
+import React from 'react';
 
 const Loader = () => {
   return (
     <div className='loader'>
         <div className="loader__image">
-            <img src={LoadingGif} alt="" />
+            <div className="tenor-gif-embed" data-postid="25192894" data-share-method="host" data-aspect-ratio="1" data-width="100%">
+                <a href="https://tenor.com/view/amalie-steiness-borregaard-loading-gif-loading-gif-25192894">Amalie Steiness GIF</a> from <a href="https://tenor.com/search/amalie-gifs">Amalie GIFs</a>
+            </div>
+            <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
         </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
