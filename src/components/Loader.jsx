f53cafd368ff51e66f21de650652a21de1a39471
Lavish-code <ylavish07@gmail.com>
2024-09-29 02:08:13 +0530

    Your commit message describing the changes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import LoadingVideo from '../images/loading.mp4';
 
 const Loader = () => {
+  const loaderStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Adjust based on your layout
+  };
+
+  const videoStyle = {
+    maxWidth: '100%', // Ensures responsiveness
+    maxHeight: '100%', // Ensures responsiveness
+  };
+
   return (
-    <div className='loader'>
-        <div className="loader__image">
-            <img src="https://media.tenor.com/images/a651d8e9b64cb2139e5a9e1d77da18f4/tenor.gif" alt="Loading..." />
-        </div>
+    <div style={loaderStyle}>
+      <div className="loader__video">
+        <video autoPlay loop muted style={videoStyle}>
+          <source src={LoadingVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
-};
+}
 
 export default Loader;
